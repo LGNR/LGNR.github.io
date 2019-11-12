5e8501aca7b3b6bb9b908dc5f3a00dae7ea4a6e9
Luis Gregorio Navarro <lgnr.archive@gmail.com>
2019-11-12 06:20:53 -0600

    Initial commit

---
+++ HIIT/precache-manifest.508bc8c7cee0aadc8205eeed3d407637.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1cff0261cdd7298fddae1a84cff546",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a59043994d5f90be489",
+    "url": "/static/js/2.636d61d5.chunk.js"
+  },
+  {
+    "revision": "80e765568adc845cfeef",
+    "url": "/static/js/main.20a588e9.chunk.js"
+  },
+  {
+    "revision": "5534140c09803414e3e1",
+    "url": "/static/js/runtime-main.573dd6f8.js"
+  }
+]);
