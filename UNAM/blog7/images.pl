aba020ea436ed5f4ff17c032c2110b54f198149b
Luis Gregorio Navarro <lgnr.archive@gmail.com>
2020-08-14 16:45:55 -0500

    Blog 7, subir imágenes

---
+++ UNAM/blog7/images.pl
@@ -0,0 +1,106 @@
+# LaTeX2HTML 2019 (Released January 1, 2019)
+# Associate images original text with physical files.
+
+
+$key = q/L_1={vec{x}inmathbb{R}^3:vec{x}=vec{u}t+vec{a}};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.69ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img23.svg"
+ ALT="$L_1=\{\vec{x}\in \mathbb{R}^3 : \vec{x}=\vec{u}t+\vec{a}\}$">|; 
+
+$key = q/L_2={vec{x}inmathbb{R}^3:vec{x}=vec{v}t+vec{b}};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 3.07ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img24.svg"
+ ALT="$L_2=\{\vec{x}\in \mathbb{R}^3 : \vec{x}=\vec{v}t+\vec{b}\}$">|; 
+
+$key = q/bullet;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.26ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img1.svg"
+ ALT="$\bullet$">|; 
+
+$key = q/delta=sqrt{epsilon};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.64ex; vertical-align: -0.67ex; " SRC="|."$dir".q|img8.svg"
+ ALT="$\delta = \sqrt{\epsilon}$">|; 
+
+$key = q/delta>0;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.93ex; vertical-align: -0.21ex; " SRC="|."$dir".q|img5.svg"
+ ALT="$\delta&gt;0$">|; 
+
+$key = q/displaystylelim_{(0,y)to(0,0)}f(x,y)=lim_{yto0}frac{0}{0+y^2}=0;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 5.53ex; vertical-align: -2.36ex; " SRC="|."$dir".q|img17.svg"
+ ALT="$\displaystyle \lim_{(0,y) \to (0,0)} f(x,y)=\lim_{y \to 0}\frac{0}{0+y^2}=0$">|; 
+
+$key = q/displaystylelim_{(0,y)to(0,0)}f(x,y)neqlim_{(x,0)to(0,0)}f(x,y)neqlim_{(x,x)to(0,0)}f(x,y)neqlim_{(x,mx)to(0,0)}f(x,y);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 4.20ex; vertical-align: -2.36ex; " SRC="|."$dir".q|img22.svg"
+ ALT="$\displaystyle \lim_{(0,y) \to (0,0)} f(x,y) \neq \lim_{(x,0) \to (0,0)} f(x,y) \neq \lim_{(x,x) \to (0,0)} f(x,y) \neq \lim_{(x,mx) \to (0,0)} f(x,y) $">|; 
+
+$key = q/displaystylelim_{(x,0)to(0,0)}f(x,y)=lim_{xto0}frac{x^2}{x^4+0}=lim_{xto0}frac{1}{x^2}=infty;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 5.92ex; vertical-align: -2.36ex; " SRC="|."$dir".q|img18.svg"
+ ALT="$\displaystyle \lim_{(x,0) \to (0,0)} f(x,y)=\lim_{x \to 0} \frac{x^2}{x^4+0}= \lim_{x \to 0} \frac{1}{x^2} = \infty$">|; 
+
+$key = q/displaystylelim_{(x,mx)to(0,0)}f(x,y)=lim_{xto0}frac{x^2}{x^4+(mx)^2}=lim_{xto0}frac{1}{x^2+m}=frac{1}{m};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 5.92ex; vertical-align: -2.36ex; " SRC="|."$dir".q|img20.svg"
+ ALT="$\displaystyle \lim_{(x,mx) \to (0,0)} f(x,y)=\lim_{x \to 0}\frac{x^2}{x^4+(mx)^2}=\lim_{x \to 0}\frac{1}{x^2+m}=\frac{1}{m} $">|; 
+
+$key = q/displaystylelim_{(x,mx^2)to(0,0)}f(x,y)=lim_{xto0}frac{x^2}{x^4+(mx^2)^2}=lim_{xto0}frac{1}{x^2(1+m^2)}=infty;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 5.92ex; vertical-align: -2.36ex; " SRC="|."$dir".q|img21.svg"
+ ALT="$\displaystyle \lim_{(x,mx^2) \to (0,0)} f(x,y)=\lim_{x \to 0}\frac{x^2}{x^4+(mx^2)^2}=\lim_{x \to 0}\frac{1}{x^2(1+m^2)}=\infty $">|; 
+
+$key = q/displaystylelim_{(x,x)to(0,0)}f(x,y)=lim_{xto0}frac{x^2}{x^4+x^2}=lim_{xto0}frac{1}{x^2+1}=1;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 5.92ex; vertical-align: -2.36ex; " SRC="|."$dir".q|img19.svg"
+ ALT="$\displaystyle \lim_{(x,x) \to (0,0)} f(x,y)=\lim_{x \to 0}\frac{x^2}{x^4+x^2}=\lim_{x \to 0}\frac{1}{x^2+1}=1$">|; 
+
+$key = q/displaystylelim_{vec{x}tovec{0}}frac{x^2y}{x^2+x^4}=lim_{vec{x}tovec{0}}frac{slashed{x^2}y}{(slashed{x^2})(1+x^2)}=frac{0}{1+0}=0;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 6.32ex; vertical-align: -2.55ex; " SRC="|."$dir".q|img10.svg"
+ ALT="$\displaystyle \lim_{\vec{x} \to\vec{0}} \frac{x^2 y}{x^2+x^4}= \lim_{\vec{x} \to\vec{0}} \frac{\slashed{x^2} y}{(\slashed{x^2})(1+x^2)}=\frac{0}{1+0}=0$">|; 
+
+$key = q/displaystylelvertlvertvec{x}-vec{0}rvertrvert=lvertlvertvec{x}rvertrvert=sqrt{x^2+y^2}<delta;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 3.14ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img7.svg"
+ ALT="$\displaystyle \lvert \lvert \vec{x} - \vec{0} \rvert \rvert=\lvert \lvert \vec{x} \rvert \rvert=\sqrt{x^2+y^2}&lt;\delta$">|; 
+
+$key = q/displaystylesqrt{x^2+y^2}<delta=sqrt{epsilon}implieslvertx^2+y^2rvert=lvertf(x,y)-0rvert<epsilon;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 3.14ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img9.svg"
+ ALT="$\displaystyle \sqrt{x^2+y^2}&lt;\delta=\sqrt{\epsilon} \implies \lvert x^2+y^2 \rvert = \lvert f(x,y) - 0 \rvert &lt; \epsilon$">|; 
+
+$key = q/displaystyle{lim_{vec{x}tovec{0}}}f(x,y)=0;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 4.10ex; vertical-align: -2.25ex; " SRC="|."$dir".q|img3.svg"
+ ALT="$\displaystyle{\lim_{\vec{x} \to\vec{0}}}f(x,y) = 0$">|; 
+
+$key = q/epsilon>0;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.81ex; vertical-align: -0.21ex; " SRC="|."$dir".q|img4.svg"
+ ALT="$\epsilon &gt; 0$">|; 
+
+$key = q/f(x,y)=frac{x^2}{x^4+y^2};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 3.70ex; vertical-align: -1.23ex; " SRC="|."$dir".q|img16.svg"
+ ALT="$f(x,y)=\frac{x^2}{x^4+y^2}$">|; 
+
+$key = q/f(x,y)=x^2+y^2;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.69ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img2.svg"
+ ALT="$f(x,y) = x^2+y^2$">|; 
+
+$key = q/lvertlvertvec{x}-vec{0}rvertrvert<deltaimplieslvertf(x,y)-(0)rvert<epsilon;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.96ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img6.svg"
+ ALT="$\lvert \lvert \vec{x} - \vec{0} \rvert \rvert&lt; \delta \implies \lvert f(x,y) - (0) \rvert &lt; \epsilon$">|; 
+
+$key = q/vec{w}=vec{u}timesvec{v};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.08ex; vertical-align: -0.31ex; " SRC="|."$dir".q|img25.svg"
+ ALT="$\vec{w}=\vec{u} \times \vec{v}$">|; 
+
+$key = q/x=0;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.72ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img11.svg"
+ ALT="$x=0$">|; 
+
+$key = q/x=y;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.68ex; vertical-align: -0.57ex; " SRC="|."$dir".q|img13.svg"
+ ALT="$x=y$">|; 
+
+$key = q/y=0;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.17ex; vertical-align: -0.57ex; " SRC="|."$dir".q|img12.svg"
+ ALT="$y=0$">|; 
+
+$key = q/y=mx;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.68ex; vertical-align: -0.57ex; " SRC="|."$dir".q|img14.svg"
+ ALT="$y=mx$">|; 
+
+$key = q/y=mx^2;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.57ex; vertical-align: -0.57ex; " SRC="|."$dir".q|img15.svg"
+ ALT="$y=mx^2$">|; 
+
+1;
+
